45e2ca33621397c5c4099e027982460afe95f772
chima <chimajunior14@gmail.com>
2025-07-06 19:43:15 +0100

    added arrays of projects

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,3 +1,35 @@
+
+const projects = [
+    {
+        id: 1,
+        title: "Movie Database Platform",
+        description: "A Beautiful database platform with user ratings, reviews and personalised reccomendations",
+        image: "/public/projects/CMDB.png",
+        tags:["Remix", "React", "Tailwind", "Express", "MySQL"],
+        demoUrl: "https://cmbd-movies.onrender.com/", 
+        githubUrl: "https://github.com/Chimajunior/CMBD_FULLSTACK"
+    },
+    {
+        id: 2,
+        title: "Movie Database Platform",
+        description: "A Beautiful database platform with user ratings, reviews and personalised reccomendations",
+        image: "/public/projects/CMDB.png",
+        tags:["Remix", "React", "Tailwind", "Express", "MySQL"],
+        demoUrl: "https://cmbd-movies.onrender.com/", 
+        githubUrl: "#"
+    },    
+    {
+        id: 3,
+        title: "Movie Database Platform",
+        description: "A Beautiful database platform with user ratings, reviews and personalised reccomendations",
+        image: "/public/projects/CMDB.png",
+        tags:["Remix", "React", "Tailwind", "Express", "MySQL"],
+        demoUrl: "https://cmbd-movies.onrender.com/", 
+        githubUrl: "#"
+    },
+];
+
+
 export const ProjectsSection = () =>{
     return <div></div>;
 
