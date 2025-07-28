1c4d847674f84bf55cffc96d5eb6cd3db4087c14
chima <chimajunior14@gmail.com>
2025-07-28 21:15:56 +0100

    resize for generate stars

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -8,46 +8,20 @@ export const StarBackground = () => {
   const [fallingStars, setFallingStars] = useState([]);
 
   useEffect(() => {
-    // generateStars();
     generateFallingStars();
 
     const handleResize = () => {
-        generateStars();
-    }
+      generateFallingStars();
+    };
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener("resize", handleResize )
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
-
-    // stars background with animation
-  const generateStars = () => {
-    const numberOfStars = Math.floor(
-      (window.innerWidth * window.innerHeight) / 13000
-    );
-
-    const newStars = [];
-
-    // looping through the amount of stars
-    for (let i = 0; i < numberOfStars; i++) {
-      newStars.push({
-        id: i,
-        size: Math.random() * 1 + 1,
-        x: Math.random() * 100,
-        y: Math.random() * 100,
-        opacity: Math.random() * 0.5 + 0.5,
-        animationDuration: Math.random() * 4 + 2,
-      });
-    }
-
-    setStars(newStars);
-  };
 
   //   Falling stars added to the background with animation
   const generateFallingStars = () => {
     const numberOfFallingStars = 4;
     const newFallingStars = [];
-
-  
 
     // looping through the amount of falling stars
     for (let i = 0; i < numberOfFallingStars; i++) {
@@ -66,21 +40,6 @@ export const StarBackground = () => {
 
   return (
     <div className="fixed inset-0  overflow-hidden pointer-events-none z-0">
-      {/* {stars.map((star) => (
-        <div
-          key={star.id}
-          className="star animate-pulse-subtle"
-          style={{
-            width: star.size + "px",
-            height: star.size + "px",
-            left: star.x + "%",
-            top: star.y + "%",
-            opacity: star.opacity,
-            animationDuration: star.animationDuration + "s",
-          }}
-        />
-      ))} */}
-
       {fallingStars.map((fallingStar) => (
         <div
           key={fallingStar.id}
@@ -90,7 +49,7 @@ export const StarBackground = () => {
             height: fallingStar.size * 2 + "px",
             left: fallingStar.x + "%",
             top: fallingStar.y + "%",
-            animationDelay: fallingStar.delay,
+            animationDelay: fallingStar.delay + "s",
             animationDuration: fallingStar.animationDuration + "s",
           }}
         />
